90a68346c50cd342ab54984a55aae040b80c2fd3
0rcl <105236460+0rcl@users.noreply.github.com>
2022-05-28 14:49:31 +0300

    v7 minor changes

--- src/main/java/Dbotpackage/nCommands/Calculate.java
+++ src/main/java/Dbotpackage/nCommands/Calculate.java
@@ -10,21 +10,21 @@ public class Calculate extends ListenerAdapter {
 
         String[] message = event.getMessage().getContentRaw().split(" ");
 
-        if(message.length == 1 && message[0].equalsIgnoreCase("!calculate")){
+        if(message.length == 1 && message[0].equalsIgnoreCase("$calculate")){
             event.getChannel().sendMessage("To use this command type(without brackets): !calculate [add/sub] [first-num] [second-num]").queue();
-        }else if(message[0].equalsIgnoreCase("!calculate") && message[1].equalsIgnoreCase("add")){
+        }else if(message[0].equalsIgnoreCase("$calculate") && message[1].equalsIgnoreCase("add")){
             int num1 = Integer.parseInt(message[2]);
             int num2 = Integer.parseInt(message[3]);
             event.getChannel().sendMessage("The Result is: " + (num1 + num2)).queue();
-        }else if(message[0].equalsIgnoreCase("!calculate") && message[1].equalsIgnoreCase("sub")){
+        }else if(message[0].equalsIgnoreCase("$calculate") && message[1].equalsIgnoreCase("sub")){
             int num1 = Integer.parseInt(message[2]);
             int num2 = Integer.parseInt(message[3]);
             event.getChannel().sendMessage("The Result is: " + (num1 - num2)).queue();
-        } else if(message[0].equalsIgnoreCase("!calculate") && message[1].equalsIgnoreCase("mul")){
+        } else if(message[0].equalsIgnoreCase("$calculate") && message[1].equalsIgnoreCase("mul")){
             int num1 = Integer.parseInt(message[2]);
             int num2 = Integer.parseInt(message[3]);
             event.getChannel().sendMessage("The Result is: " + (num1 * num2)).queue();
-        } else if(message[0].equalsIgnoreCase("!calculate") && message[1].equalsIgnoreCase("div")) {
+        } else if(message[0].equalsIgnoreCase("$calculate") && message[1].equalsIgnoreCase("div")) {
             int num1 = Integer.parseInt(message[2]);
             int num2 = Integer.parseInt(message[3]);
             event.getChannel().sendMessage("The Result is: " + (num1 / num2)).queue();
